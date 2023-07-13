e5f0496010848ee9ce51427ae5f968aee6413a8c
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-13 15:01:34 +0545

    feat: Add Product component

---
+++ src/components/Product.jsx
@@ -0,0 +1,15 @@
+
+export default function Product({id, img, title, subTitle, price}) {
+    return (
+        <div className="product-item" key={id}>
+            <div className="product-header">
+                <img src={img} alt="product" className="img-fluid" />
+            </div>
+            <div className="product-card p-2">
+                <p className="product-title m-0"> {title} </p>
+                <p className="product-sub-title"> {subTitle} </p>
+                <span className="product-price" style={{fontWeight: "bold"}}> ${price} </span>
+            </div>
+        </div>
+    )
+}
