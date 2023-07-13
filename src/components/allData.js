bb09ba41dd7dcc858cb1edd284880210eaeb7e42
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-13 09:25:49 +0545

    Added allData

---
+++ src/components/allData.js
@@ -0,0 +1,325 @@
+import img1 from "../assets/img/product/afdior.png"
+import otherImg1 from "../assets/img/product/afdior1.png"
+import otherImg2 from "../assets/img/product/afdior2.png"
+import img2 from "../assets/img/product/afdiorlow.png"
+import otherImg3 from "../assets/img/product/afdiorlow1.png"
+import otherImg4 from "../assets/img/product/afdiorlow2.png"
+import img3 from "../assets/img/product/afspid.jpg"
+import otherImg5 from "../assets/img/product/afspid.webp"
+import otherImg6 from "../assets/img/product/afspid1.jpeg"
+import img4 from "../assets/img/product/afzoom.webp"
+import otherImg7 from "../assets/img/product/afzoom1.webp"
+import otherImg8 from "../assets/img/product/afzoom2.webp"
+import img5 from "../assets/img/product/airflv.png"
+import otherImg9 from "../assets/img/product/airflv1.png"
+import otherImg10 from "../assets/img/product/airflv2.png"
+import img6 from "../assets/img/product/airforcelr.webp"
+import otherImg11 from "../assets/img/product/airforcelr1.webp"
+import otherImg12 from "../assets/img/product/airforcelr2.webp"
+import img7 from "../assets/img/product/airforcew.webp"
+import otherImg13 from "../assets/img/product/airforcew1.webp"
+import otherImg14 from "../assets/img/product/airforcew2.webp"
+import img8 from "../assets/img/product/cap.webp"
+import otherImg15 from "../assets/img/product/cap1.webp"
+import otherImg16 from "../assets/img/product/cap2.webp"
+import img9 from "../assets/img/product/lowshort1.jpg"
+import otherImg17 from "../assets/img/product/lowshort2.webp"
+import otherImg18 from "../assets/img/product/lowshort3.jpg"
+import img10 from "../assets/img/product/nblretro.webp"
+import otherImg19 from "../assets/img/product/nblretro2.webp"
+import otherImg20 from "../assets/img/product/nblretro3.webp"
+import img11 from "../assets/img/product/nblretropre.webp"
+import otherImg21 from "../assets/img/product/nblretropre1.webp"
+import otherImg22 from "../assets/img/product/nblretropre2.webp"
+import img12 from "../assets/img/product/panda.webp"
+import otherImg23 from "../assets/img/product/panda.webp"
+import otherImg24 from "../assets/img/product/panda.webp"
+import img13 from "../assets/img/product/short-sleeve.webp"
+import otherImg25 from "../assets/img/product/short-sleeve1.webp"
+import otherImg26 from "../assets/img/product/short-sleeve2.webp"
+import img14 from "../assets/img/product/shorts.webp"
+import otherImg27 from "../assets/img/product/shorts1.webp"
+import otherImg28 from "../assets/img/product/shorts2.webp"
+import img15 from "../assets/img/product/SqueezableBottle.webp"
+import otherImg29 from "../assets/img/product/SqueezableBottle1.webp"
+import otherImg30 from "../assets/img/product/SqueezableBottle2.webp"
+import img16 from "../assets/img/product/sunglass.webp"
+import otherImg31 from "../assets/img/product/sunglass1.webp"
+import otherImg32 from "../assets/img/product/sunglass2.webp"
+import img17 from "../assets/img/product/tbackpack.webp"
+import otherImg33 from "../assets/img/product/tbackpack1.webp"
+import otherImg34 from "../assets/img/product/tbackpack2.webp"
+import img18 from "../assets/img/product/travis.png"
+import otherImg35 from "../assets/img/product/travis1.webp"
+import otherImg36 from "../assets/img/product/travis2.webp"
+import img19 from "../assets/img/product/womenjack.webp"
+import otherImg37 from "../assets/img/product/womenjack1.webp"
+
+export const items = [
+    {
+        id: 1,
+        category: "sneaker",
+        subCategory: "jordon",
+        description: "Jordan Brand connected with Parisian fashion house Dior to create history with the Jordan 1 Retro High Dior, now available on store. This is the first time that Jordan has collaborated with a legacy fashion label like Dior, making this release one for the books. This release was limited to only 8,500 pairs, each pair individually numbered.",
+        img: img1,
+        name: "Jordan 1 Retro High Dior",
+        subName: "Dior",
+        price: 7257,
+        otherImgs: [otherImg1, otherImg2],
+        style: "CN8607 002",
+        colorway: "WOLF GREY/SAIL-PHOTON DUST-WHITE",
+        releaseDate: "04/06/2020"
+    },
+    {
+        id: 2,
+        category: "sneaker",
+        subCategory: "jordon",
+        description: "Jordan Brand connected with Parisian fashion house Dior to create history with the Jordan 1 Retro Low Dior, now available on store. This is the first time that Jordan has collaborated with a legacy fashion label like Dior, making this release one for the books. This release was limited to only 4,700 pairs, each pair individually numbered",
+        img: img2,
+        name: "Jordan 1 Retro Low Dior",
+        subName: "Dior",
+        price: 5914,
+        otherImgs: [otherImg3, otherImg4],
+        style: "CN8608 002",
+        colorway: "GREY/BLACK-SAIL",
+        releaseDate: "04/06/2020"
+    },
+    {
+        id: 3,
+        category: "sneaker",
+        subCategory: "jordon",
+        description: "Nike and Jordan Brand are returning back to the Spider-Verse for their second Spider-Man themed Air Jordan 1, with the release of the Air Jordan 1 High OG Spider-Man Across the Spider-Verse.",
+        img: img3,
+        name: "Jordan 1 Retro High OG",
+        subName: "Spider-Man Across the Spider-Verse",
+        price: 306,
+        otherImgs: [otherImg5, otherImg6],
+        style: "DV1748-601",
+        colorway: "UNIVERSITY RED/BLACK/WHITE",
+        releaseDate: "05/20/2023"
+    },
+    {
+        id: 4,
+        category: "sneaker",
+        subCategory: "jordon",
+        description: "Turn style on its head with this crafted take on the Air Jordan 1 Mid. Its 'inside out'-inspired construction, including unique layering and exposed foam accents, ups the ante on this timeless Jordan Brand silhouette. ",
+        img: img4,
+        name: "Air Jordon 1 Mid",
+        subName: "SE Craft",
+        price: 135,
+        otherImgs: [otherImg7, otherImg8],
+        style: "DM9652-102",
+        colorway: "Sail/Fossil Stone/Celestial Gold/Taupe Haze",
+        releaseDate: "04/01/2023"
+    },
+    {
+        id: 5,
+        category: "sneaker",
+        subCategory: "airforce",
+        description: "Pre-orders for the AF1 opened to select Louis Vuitton clients. And on July 19, nine editions of Louis Vuitton's Air Force 1 released online to the public, each priced between $2,750 and $3,450, with an in-store drop reportedly in the works.",
+        img: img5,
+        name: "Louis Vuitton Nike Air Force 1 Low",
+        subName: "By Virgil Abloh White Red",
+        price: 4077,
+        otherImgs: [otherImg9, otherImg10],
+        style: "LV",
+        colorway: "WHITE/RED",
+        releaseDate: "07/19/2022"
+    },
+    {
+        id: 6,
+        category: "sneaker",
+        subCategory: "airforce",
+        description: "Debuting in 1982 as a basketball must-have, the Air Force 1 came into its own in the '90s. The clean look of the classic white-on-white AF1 was endorsed from the basketball courts to the block and beyond. Finding its rhythm in hip-hop culture, releasing limited collabs and colorways, Air Force 1 became an iconic sneaker around the globe. And with over 2000 iterations of this staple, its impact on fashion, music and sneaker culture can’t be denied.",
+        img: img6,
+        name: "Nike Air Force 1Low Retro",
+        subName: "Men's Shoes",
+        price: 150,
+        otherImgs: [otherImg11, otherImg12],
+        style: "FD7039-100",
+        colorway: "White/Gum Yellow/University Gold",
+        releaseDate: ""
+    },
+    {
+        id: 7,
+        category: "sneaker",
+        subCategory: "airforce",
+        description: "The radiance lives on in the Nike Air Force 1 ’07, the b-ball OG that puts a fresh spin on what you know best: durably stitched overlays, clean finishes and the perfect amount of flash to make you shine.",
+        img: img7,
+        name: "Nike Air Force 1'07",
+        subName: "Men's Shoes",
+        price: 150,
+        otherImgs: [otherImg13, otherImg14],
+        style: "CW2288-111",
+        colorway: "White/White",
+        releaseDate: "2015"
+    },
+    {
+        id: 8,
+        category: "accessories",
+        subCategory: "cap",
+        description: "A classic mid-depth cap with plenty of styling options, this Nike Club Cap comes in smooth cotton twill that has a soft wash for easy comfort from day 1. The precurved bill lends itself to casual styling, and the adjustable back-strap lets you find the right fit.",
+        img: img8,
+        name: "Nike Club",
+        subName: "Unstructured Futura Wash Cap",
+        price: 26,
+        otherImgs: [otherImg15, otherImg16],
+        style: "FB5368-100",
+        colorway: "White/Black",
+        releaseDate: ""
+    },
+    {
+        id: 9,
+        category: "allclothing",
+        subCategory: "short",
+        description: "The Nike Flex Stride Shorts get updated with an all-new woven fabric and enhanced breathability in high-sweat areas. Their soft liner provides secure support where you need it. This product is made with at least 50% recycled polyester fibers.",
+        img: img9,
+        name: "Nike Flex Stride",
+        subName: `Men's 5" 2-In-1 Running Shorts`,
+        price: 55,
+        otherImgs: [otherImg17, otherImg18],
+        style: "CJ5467-010",
+        colorway: "Black/Black",
+        releaseDate: ""
+    },
+    {
+        id: 10,
+        category: "sneaker",
+        subCategory: "dunks",
+        description: "From backboards to skateboards, the influence of the Nike Dunk is undeniable. Although introduced as a basketball shoe in 1985, its flat and grippy soles were perfect for a neglected sports community—skaters. Uncovering a subculture craving creativity as much as function, Dunk released decades of countless colorways that continue to capture the soul of skaters from coast to coast.",
+        img: img10,
+        name: "Nike Dunk Low LX",
+        subName: "Women's Shoes",
+        price: 120,
+        otherImgs: [otherImg19, otherImg20],
+        style: "DV7411-200",
+        colorway: "Celestial Gold/Sail/Gold Suede/Wheat Gold",
+        releaseDate: ""
+    },
+    {
+        id: 11,
+        category: "sneaker",
+        subCategory: "dunks",
+        description: "Fireside style and a whole lotta freshness—this joyful take on the Dunk Low is the perfect match for rosy cheeks, the first floating snowflakes and your favorite winter beverage. Plaid accents bring the warm feels, while cheerful colors put a seasonally appropriate bow on top of the storied basketball look. Don't worry—even if you've been naughty, you're gonna look nice.",
+        img: img11,
+        name: "Nike Dunk Low Retro Premium",
+        subName: "Men's Shoes",
+        price: 120,
+        otherImgs: [otherImg21, otherImg22],
+        style: "DV0827-100",
+        colorway: "White/White/University Red/Black",
+        releaseDate: ""
+    },
+    {
+        id: 12,
+        category: "sneaker",
+        subCategory: "dunks",
+        description: "From the school-spirited College Colors Program to the vibrant Nike CO.JP collection, Nike Dunks have seen many colorways since the design’s inception in 1985. But with each new colorway, the Dunk’s classic color-blocking has remained in some capacity. Nike put its timeless color-blocking to work with the Nike Dunk Low Retro White Black.",
+        img: img12,
+        name: "Nike Dunk Low Retro",
+        subName: "White Black Panda (2021)",
+        price: 122,
+        otherImgs: [otherImg23, otherImg24],
+        style: "DD1391-100",
+        colorway: "White/Black",
+        releaseDate: "03/10/2021"
+    },
+    {
+        id: 13,
+        category: "allclothing",
+        subCategory: "tshirt",
+        description: "Run, spin, lift, stretch—no matter where your workout takes you, we have you covered in the sweat-wicking Primary Tank. It's ultra soft and comfortable, with underarm ventilation to help keep you cool during your reps.",
+        img: img13,
+        name: "Nike Primary",
+        subName: "Men'Dri-FIT Versatile Tank",
+        price: 30,
+        otherImgs: [otherImg25, otherImg26],
+        style: "DV9833-815",
+        colorway: "Monarch/Heather",
+        releaseDate: ""
+    },
+    {
+        id: 14,
+        category: "allclothing",
+        subCategory: "short",
+        description: "The Nike Sportswear Club shorts is made with our everyday cotton fabric and a classic fit for a familiar feel right out of the bag. An embroidered Futura logo on the chest provides a signature Nike look.",
+        img: img14,
+        name: "Nike Sportswear ",
+        subName: "Men's Graphic Shorts",
+        price: 30,
+        otherImgs: [otherImg27, otherImg28],
+        style: "AR4997-591",
+        colorway: "Violet Shock",
+        releaseDate: ""
+    },
+    {
+        id: 15,
+        category: "accessories",
+        subCategory: "bottle",
+        description: "A water bottle made to go with you no matter where your workout takes you. This textured, squeezable bottle fits comfortably in your hand, making hydration a breeze from warm-up to cooldown.",
+        img: img15,
+        name: "Nike Refuel ",
+        subName: "Men's Graphic Shorts",
+        price: 15,
+        otherImgs: [otherImg29, otherImg30],
+        style: "N1007666-125",
+        colorway: "Grey",
+        releaseDate: ""
+    },
+    {
+        id: 16,
+        category: "accessories",
+        subCategory: "sunglass",
+        description: "Throw back your style without compromising on comfort. With their '90s vibe, the Nike NV07 Sunglasses have a square, streamlined design without giving up on durability and wearability. Between the 4-base flat lens providing a slim profile and the durable hinges, you'll be made in the shade.",
+        img: img16,
+        name: "Nike NV07 ",
+        subName: "Sunglasses",
+        price: 100,
+        otherImgs: [otherImg31, otherImg32],
+        style: "FN0303-010",
+        colorway: "Black/Dark Grey/Dark Grey",
+        releaseDate: ""
+    },
+    {
+        id: 17,
+        category: "accessories",
+        subCategory: "backpack",
+        description: "The Nike Utility Speed Backpack keeps your gear close, secure and organized when commuting to and from training sessions. Cushioned straps give you comfort on the go, and the pack opens flat for easy access to must-have items.",
+        img: img17,
+        name: "Nike Utility Speed",
+        subName: "Training Backpack (27L)",
+        price: 77,
+        otherImgs: [otherImg33, otherImg34],
+        style: "CK2668-010",
+        colorway: " Black/Black/Enigma Stone",
+        releaseDate: ""
+    },
+    {
+        id: 18,
+        category: "sneaker",
+        subCategory: "dunks",
+        description: "ravis Scott teamed up with Nike SB to release his first official skate shoe, the Nike SB Dunk Low Travis Scott (Regular Box), now available on store. This design follows a similar design aesthetic as seen on the Air Force 1 Low Travis Scott Cactus Jack, featuring an array of materials and prints. Unlike previous Travis Scott releases, these were not available on SNKRS and only available at select Nike SB stockists. ",
+        img: img18,
+        name: "Nike SB Dunk Low",
+        subName: "Travis Scott (Regular Box)",
+        price: 1200,
+        otherImgs: [otherImg35, otherImg36],
+        style: "CT5053-001",
+        colorway: "Black/Black-Parachute Beige-Petra Brown",
+        releaseDate: "02/29/2020"
+    },
+    {
+        id: 19,
+        category: "allclothing",
+        subCategory: "jacket",
+        description: "This premium leather jacket is slightly cropped with a snug fit. Satin lining, a snap at the collar and zippers on the sleeves combine modern with moto. Exclusive original graphics are featured on embroidered and rubber patches throughout.",
+        img: img19,
+        name: "Jordan x Travis Scott",
+        subName: "Women's Jacket",
+        price: 600,
+        otherImgs: [otherImg37],
+        style: "DX6168-256",
+        colorway: "Archaeo Brown/Dark Smoke Grey/Sail",
+        releaseDate: ""
+    },
+];
