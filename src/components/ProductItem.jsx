8510cbfc91625b07e0fb9e077a9bbd3687b59991
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-13 15:02:01 +0545

    feat : Add ProductItem component

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,25 @@
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Product from "./Product";
 import items from "./allData";
 
-
-export default function ProductItem(){
-    return(
+export default function ProductItem() {
+    return (
         <>
-            <p>produtItems</p>
+            <Container>
+                <Row>
+                    {
+                        items.map((item) => {
+                            return (
+                                <Col key={item.id} lg={3} md={6} >
+                                    <Product img={item.img} title={item.name} subTitle={item.subName} price={item.price} id={item.id} />
+                                </Col>
+                            )
+                        })
+                    }
+                </Row>
+            </Container>
         </>
     );
 }
