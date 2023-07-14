6ea913eac01cae3262b42d900816ede79f205a19
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-14 09:14:28 +0545

    feat : Add routes to component and dyanmic params for current URL

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import Home from "./pages/Home"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductPage from "./pages/ProductPage";
 
 function App() {
 
   return (
     <>
-    <Home />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories/product/:id" element={<ProductPage />} />
+    </Routes>
     </>
   )
 }
