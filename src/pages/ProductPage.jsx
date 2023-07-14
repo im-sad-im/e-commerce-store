6bf90839ddf303cc7184d91c49952c8ca6e88bf7
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-14 09:16:14 +0545

    feat : Add id param and filter items' id with param id

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -3,9 +3,13 @@ import items from "../components/allData";
 import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import { useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProductPage() {
-    const [image, setImage] = useState(items[0].img);
+    const { id } = useParams();
+    const item = items.filter((item)=> item.id === parseInt(id));
+
+    const [image, setImage] = useState(item[0].img);
 
     const handleMouseOver = (e) => {
         setImage(e.target.src);
@@ -23,25 +27,25 @@ export default function ProductPage() {
                                     <img src={image} alt="product image" className="img-fluid" />
                                 </div>
                                 <div className="alternate-img d-flex w-25 mt-4 mx-4">
-                                    <img src={items[0].img} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
-                                    <img src={items[0].otherImgs[0]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
-                                    <img src={items[0].otherImgs[1]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
+                                    <img src={item[0].img} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
+                                    <img src={item[0].otherImgs[0]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
+                                    <img src={item[0].otherImgs[1]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
                                 </div>
                             </div>
                         </Col>
                         <Col lg={5}>
                             <div className="right">
-                                <h2>{items[0].name}</h2>
-                                <h3>{items[0].subName}</h3>
+                                <h2>{item[0].name}</h2>
+                                <h3>{item[0].subName}</h3>
                                 <div className="product-spec">
                                     <div className="product-des">
-                                        <p> {items[0].description} </p>
+                                        <p> {item[0].description} </p>
                                     </div>
                                     <div className="product-detail">
-                                        <p className=""> Style : {items[0].style} </p>
-                                        <p className=""> Colorway : {items[0].colorway} </p>
-                                        {(items[0].releaseDate != "") &&
-                                            <p className=""> Release Date : {items[0].releaseDate} </p>
+                                        <p className=""> Style : {item[0].style} </p>
+                                        <p className=""> Colorway : {item[0].colorway} </p>
+                                        {(item[0].releaseDate != "") &&
+                                            <p className=""> Release Date : {item[0].releaseDate} </p>
                                         }
                                     </div>
                                 </div>
