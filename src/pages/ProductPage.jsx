5de04c0d74646b5dfefd3e886fcb57f2a1890e03
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-14 17:46:10 +0545

    Modify ProductPage.jsx

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,9 @@
-import Container from "react-bootstrap/esm/Container";
-import items from "../components/allData";
-import Row from "react-bootstrap/esm/Row";
-import Col from "react-bootstrap/esm/Col";
 import { useState } from "react";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
 import { useParams } from "react-router-dom";
+import items from "../components/allData";
 
 export default function ProductPage() {
     const { id } = useParams();
@@ -23,13 +23,13 @@ export default function ProductPage() {
                     <Row gap={10}>
                         <Col lg={7}>
                             <div className="left mb-sm-5">
-                                <div className="main-img">
-                                    <img src={image} alt="product image" className="img-fluid" />
+                                <div className="main-img w-100 d-flex justify-content-center">
+                                    <img src={image} alt="product image" className="w-75 h-100 object-cover-fit" />
                                 </div>
-                                <div className="alternate-img d-flex w-25 mt-4 mx-4">
-                                    <img src={item[0].img} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
-                                    <img src={item[0].otherImgs[0]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
-                                    <img src={item[0].otherImgs[1]} alt="product image" className="w-50" onMouseOver={handleMouseOver} />
+                                <div className="alternate-img d-flex w-100 justify-content-center align-item-center py-3 gap-3">
+                                    <img src={item[0].img} alt="product image" className="w-19 object-fit-cover h-25" onMouseOver={handleMouseOver} />
+                                    <img src={item[0].otherImgs[0]} alt="product image" className="w-19 object-fit-cover h-25" onMouseOver={handleMouseOver} />
+                                    <img src={item[0].otherImgs[1]} alt="product image" className="w-19 object-fit-cover h-25" onMouseOver={handleMouseOver} />
                                 </div>
                             </div>
                         </Col>
