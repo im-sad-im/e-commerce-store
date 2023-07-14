8f3703e8a0bb1b2bf5bac1bf70002235101a24fc
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-14 09:17:38 +0545

    feat : Add Link to product route onClick

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,6 +3,7 @@ import Row from "react-bootstrap/esm/Row";
 import Col from "react-bootstrap/esm/Col";
 import Product from "./Product";
 import items from "./allData";
+import { Link } from "react-router-dom";
 
 export default function ProductItem() {
     return (
@@ -12,9 +13,9 @@ export default function ProductItem() {
                     {
                         items.map((item) => {
                             return (
-                                <Col key={item.id} lg={3} md={6} >
+                                <Col key={item.id} lg={3} md={6} ><Link key={item.id} onClick={() => { window.scrollTo(0, 0) }} to={`/categories/product/${item.id}`}>
                                     <Product img={item.img} title={item.name} subTitle={item.subName} price={item.price} id={item.id} />
-                                </Col>
+                                </Link> </Col>
                             )
                         })
                     }
