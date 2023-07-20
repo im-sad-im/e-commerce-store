a5e75fb710fb4e32f2dfc4bd98ea0bc226a140f0
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-20 19:18:35 +0545

    Update NavbarNav.jsx

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -1,15 +1,17 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import {AiOutlineHeart} from 'react-icons/ai';
+import {BiShoppingBag} from 'react-icons/bi';
 
 function NavbarNav() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
+    <Navbar expand="lg" className="bg-body-tertiary position-fixed w-100 top-0">
       <Container>
         <Navbar.Brand href="/">e.commerce</Navbar.Brand>
-          <div className="acct-btn">
-            <i>fav</i> {" "}
-            <i>buy</i>
+          <div className="acct-btn d-flex align-items-center gap-3  ">
+             <AiOutlineHeart size={24} />
+            <BiShoppingBag  size={24}/> 
           </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
