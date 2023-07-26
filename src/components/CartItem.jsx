35a161b1af431a393157112f1ee891eab7ea547c
Sadim <106245875+im-sad-im@users.noreply.github.com>
2023-07-26 16:28:57 +0545

    Update CartItem.jsx

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,5 +1,8 @@
+import {useContext} from 'react';
+import { CartContext } from "./pages/ProductPage";
 
 export default function CartItem(){
+    const {cartItem} = useContext(CartContext);
     return(
         <>
             <h2>Item</h2>
