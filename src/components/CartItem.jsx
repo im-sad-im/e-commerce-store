89e5408c9011790bbabf544fcbb5d96d1bf51831
Sadim Mali <sadimmalakar77@gmail.com>
2023-07-27 19:30:27 +0545

    Update CartItem

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,32 @@
-import {useContext} from 'react';
-import { CartContext } from "./pages/ProductPage";
+import { useContext } from 'react';
+import { CartContext } from "../pages/ProductPage";
 
-export default function CartItem(){
-    const {cartItem} = useContext(CartContext);
-    return(
+export default function CartItem() {
+    const { cartItem } = useContext(CartContext);
+
+    return (
         <>
-            <h2>Item</h2>
+            {
+                cartItem.map((item) => {
+                    return (
+                        <div key={item.id} className="cartItem d-flex flex-row my-5 gap-4">
+                            <div className="cart-img" style={{ maxWidth: "150px" }}>
+                                <img src={item.img} alt="product image" className='w-100' />
+                            </div>
+                            <div className="cartItem-details d-flex justify-content-between w-50">
+                                <div className="cartItem-content d-flex-column">
+                                    <h4>{item.name}</h4>
+                                    <div>{item.subName}</div>
+                                    <div>{item.colorway}</div>
+                                </div>
+                                <div className="price">
+                                    <span className='fw-bold'>${item.price}</span>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })
+            }
         </>
     )
 }
