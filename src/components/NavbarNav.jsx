b6d7458a88f6a4d5fa6d8aae5360a1c67f30443c
Sadim Mali <sadimmalakar77@gmail.com>
2023-08-01 14:35:47 +0545

    Add Link route to Cart component

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -1,22 +1,23 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {AiOutlineHeart} from 'react-icons/ai';
+import { AiOutlineHeart } from 'react-icons/ai';
 // import {BiShoppingBag} from 'react-icons/bi';
-import {PiBag} from 'react-icons/pi';
+import { PiBag } from 'react-icons/pi';
+import { Link } from 'react-router-dom';
 
 function NavbarNav() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary position-fixed w-100 top-0">
       <Container>
         <Navbar.Brand href="/">e.commerce</Navbar.Brand>
-          <div className="acct-btn d-flex align-items-center gap-3  ">
-             <AiOutlineHeart size={24} />
-             <div className='position-relative'>
-            <PiBag  size={26}/> 
-            <span className='position-absolute w-100 text-center start-0 visible fw-bold' style={{fontSize: "9px", top: "9px"}}> 1</span>
-             </div>
+        <div className="acct-btn d-flex align-items-center gap-3  ">
+          <AiOutlineHeart size={24} />
+          <div className='position-relative'>
+            <Link to="/cart"> <PiBag size={26} />
+              <span className='position-absolute w-100 text-center start-0 visible fw-bold' style={{ fontSize: "9px", top: "9px" }}> 1</span> </Link>
           </div>
+        </div>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
