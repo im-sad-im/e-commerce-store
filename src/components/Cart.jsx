40882a5612709d8daad658818553132f59a2419a
Sadim Mali <sadimmalakar77@gmail.com>
2023-08-01 14:38:18 +0545

    Render CartItem only if, items are in Cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,20 +1,31 @@
 import Container from "react-bootstrap/esm/Container";
 import CartItem from "./CartItem";
-
+import { CartContext } from "../pages/ProductPage";
+import { useContext } from "react";
 
 export default function Cart() {
+
+    const { cartItem } = useContext(CartContext);
+
     return (
         <>
             <Container className="mt-5">
-            <div className="membership border p-2">
-                <h4 style={{color: "#FF5000"}}>Members get free shipping on orders $50+</h4>
-                <p>Become a Nike Member for fast free shipping on orders $50+ <span>Join us</span> or <span>Sign-in</span> </p>
-            </div>
-            <div className="off p-2 mt-2 position-relative" style={{backgroundColor: "rgb(247, 247, 247)"}}>
-                <span className="banner-title">New Styles Added: Save Up to 60%</span>
-               <p className="banner-subtitle"> Get an extra 20% off select styles with code SCHOOL20. Ends 8.5. </p>
-            </div>
-                <CartItem />
+                <div className="membership border p-2">
+                    <h4 style={{ color: "#FF5000" }}>Members get free shipping on orders $50+</h4>
+                    <p>Become a Nike Member for fast free shipping on orders $50+ <span>Join us</span> or <span>Sign-in</span> </p>
+                </div>
+                <div className="off p-2 mt-2 position-relative" style={{ backgroundColor: "rgb(247, 247, 247)" }}>
+                    <span className="banner-title">New Styles Added: Save Up to 60%</span>
+                    <p className="banner-subtitle"> Get an extra 20% off select styles with code SCHOOL20. Ends 8.5. </p>
+                </div>
+                {/* render items if there is items in cart */}
+                {
+                    (cartItem.length > 0) ? <CartItem /> :
+                        <div className="no-item">
+                            <h3>Bag</h3>
+                            <p>There are no items in your bag.</p>
+                        </div>
+                }
             </Container>
         </>
     )
