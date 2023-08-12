34edc69e281755430e00456fdc97376768a150f4
Sadim Mali <sadimmalakar77@gmail.com>
2023-08-12 06:55:24 +0545

    Add no. of item in bag on add btn triggered

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -5,8 +6,12 @@ import { AiOutlineHeart } from "react-icons/ai";
 // import {BiShoppingBag} from 'react-icons/bi';
 import { PiBag } from "react-icons/pi";
 import { Link } from "react-router-dom";
+import { CartContext } from "../pages/ProductPage";
 
 function NavbarNav() {
+
+  const {cartItem} = useContext(CartContext);
+
   return (
     <Navbar expand="lg" className="bg-body-tertiary position-fixed w-100 top-0">
       <Container>
@@ -22,7 +27,7 @@ function NavbarNav() {
                 style={{ fontSize: "9px", top: "9px" }}
               >
                 {" "}
-                1
+                {(cartItem.length > 0) && cartItem.length}
               </span>{" "}
             </Link>
           </div>
