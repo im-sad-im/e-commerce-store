1fc183a8c06c7aa7067c4038af7acb8224563bfc
Sadim Mali <sadimmalakar77@gmail.com>
2023-08-20 14:02:28 +0545

    Add state variable titleName that updates on filteredProduct

--- src/components/Categories-pages/CategoriesHeader.jsx
+++ src/components/Categories-pages/CategoriesHeader.jsx
@@ -1,23 +1,44 @@
-import React from 'react'
-import Container from 'react-bootstrap/esm/Container'
-import { Link } from 'react-router-dom'
-
+import React, { useState } from "react";
+import Container from "react-bootstrap/esm/Container";
+import { Link } from "react-router-dom";
 
 export default function CategoriesHeader() {
+  const [titleName, setTitleName] = useState("All");
+
+  const handleTitleName = (e) => {
+    setTitleName(e);
+  };
   return (
-    <Container className='pt-5 mt-5'>    
-    <div className="catego-header d-flex flex-column ">
+    <Container className="pt-5 mt-5">
+      <div className="catego-header d-flex flex-column ">
         <div className="title-home d-flex w-100 align-items-center pb-5">
-            <Link to='/'> Home</Link>
-            <h3 className='text-center text-uppercase mx-auto'>All</h3>
+          <Link to="/"> Home</Link>
+          <h3 className="text-center text-uppercase mx-auto">{titleName}</h3>
         </div>
         <div className="filter-buttons w-100 d-flex align-items-center gap-3 justify-content-center">
-            <Link to="/categories/all"><span>All</span></Link>
-            <Link to="/categories/allclothing"><span>allclothing</span></Link>
-            <Link to="/categories/sneaker"><span>sneakers</span></Link>
-            <Link to="/categories/accessories"><span>accessories</span></Link>
+          <Link to="/categories/all" onClick={() => handleTitleName("All")}>
+            <span>All</span>
+          </Link>
+          <Link
+            to="/categories/allclothing"
+            onClick={() => handleTitleName("allclothing")}
+          >
+            <span>allclothing</span>
+          </Link>
+          <Link
+            to="/categories/sneaker"
+            onClick={() => handleTitleName("sneakers")}
+          >
+            <span>sneakers</span>
+          </Link>
+          <Link
+            to="/categories/accessories"
+            onClick={() => handleTitleName("accessories")}
+          >
+            <span>accessories</span>
+          </Link>
         </div>
-    </div>
+      </div>
     </Container>
-  )
+  );
 }
