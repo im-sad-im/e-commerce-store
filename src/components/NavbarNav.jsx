d4e3b7d8e32393e87033ad797968a5397694969d
Sadim Mali <sadimmalakar77@gmail.com>
2023-09-09 19:03:46 +0545

    Add z-index

--- src/components/NavbarNav.jsx
+++ src/components/NavbarNav.jsx
@@ -3,7 +3,6 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { AiOutlineHeart } from "react-icons/ai";
-// import {BiShoppingBag} from 'react-icons/bi';
 import { PiBag } from "react-icons/pi";
 import { Link } from "react-router-dom";
 import { CartContext } from "../pages/ProductPage";
