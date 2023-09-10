f7a258a2ea08a3df5007cb846bfd0f7c34b24045
Sadim Mali <sadimmalakar77@gmail.com>
2023-09-10 16:09:44 +0545

    Modify hover effect on product item

--- src/components/TrendingItem.jsx
+++ src/components/TrendingItem.jsx
@@ -7,12 +7,12 @@ export const TrendingItem = () => {
   return (
     <>
       {filteredItem.map((item) => (
-        <div key={item.id} className="row-item">
+        <div key={item.id} className="product-item">
           <Link to={`/categories/product/${item.id}`}>
             <div className="item-header">
               <img src={item.img} alt="product" style={{width: "15.1rem"}}/>
             </div>
-            <div className="product-title">
+            <div className="product-title px-2">
               <p>{item.name}</p>
               <p className="product-price fw-bold">{item.price}$</p>
             </div>
