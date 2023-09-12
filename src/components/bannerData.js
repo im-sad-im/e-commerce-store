3eafed1ad9ed3a2d05a74ee378f23daf60ea9291
Sadim Mali <sadimmalakar77@gmail.com>
2023-09-12 19:57:32 +0545

    Add bannerData for Banner component

---
+++ src/components/bannerData.js
@@ -0,0 +1,18 @@
+import bannerImg from "../assets/img/banner/banner.jpg";
+import bannerImg1 from "../assets/img/banner/banner1.jpg";
+
+export const bannerData = [
+    {
+        id: 1,
+        title: "Creative comfort",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, sunt inventore! Quas neque beatae quo accusamus officiis, voluptates necessitatibus delectus nobis exercitationem? Ducimus, totam!",
+        img: bannerImg
+
+    },
+    {
+        id: 1,
+        title: "Creative comfort",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, sunt inventore! Quas neque beatae quo accusamus officiis, voluptates necessitatibus delectus nobis exercitationem? Ducimus, totam!",
+        img: bannerImg1
+    }
+];
